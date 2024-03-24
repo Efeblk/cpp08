140e45444a13567d2d182613ac4b0468d4b824b4
ismet efe balik <ibalik@k1m09s08.42istanbul.com.tr>
2024-03-24 19:14:27 +0300

    finito

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -13,8 +13,12 @@ public:
     ~MutantStack() {};
 
     typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+
     iterator begin() { return std::stack<T>::c.begin(); }
     iterator end() { return std::stack<T>::c.end(); }
+    reverse_iterator rbegin() { return std::stack<T>::c.rbegin(); }
+    reverse_iterator rend() { return std::stack<T>::c.rend(); }
 };
 
 #endif
